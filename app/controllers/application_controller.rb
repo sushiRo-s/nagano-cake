8099331338f0dcebbd530f52d378fc0591213316
reiko-ishihara <violet794.py16@gmail.com>
2021-05-18 13:06:12 +0000

    [update]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,26 +10,26 @@ class ApplicationController < ActionController::Base
     def after_log_in_path_for(resource)
       admin_path(resource)
     end
-    
-    
+
+
   before_action:authenticate_member!,except: [:top, :about]
 
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_in_path_for(resource)
-      member_path(resource)
+      public_member_path(resource)
     end
 
     def after_log_in_path_for(resource)
-      member_path(resource)
+      public_member_path(resource)
     end
-  
-    
-    
+
+
+
   protected
 
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
     end
-  
+
 end
