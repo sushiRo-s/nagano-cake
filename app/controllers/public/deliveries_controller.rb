445959dd8ea9daf63d4c7bc7eb99d0414dc7f3ed
reiko-ishihara <violet794.py16@gmail.com>
2021-05-19 08:17:15 +0000

    aa

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -1,26 +1,42 @@
 class Public::DeliveriesController < ApplicationController
-  
-  
+
+
   def index
     @delivery = Delivery.new
     @deliverys = Delivery.all
   end
-  
+
   def create
+    delivery = Delivery.new(delivery_params)
+    delivery.save
+    redirect_to 'index'
   end
-  
+
   def destroy
+    
   end
-  
+
   def edit
+    @delivery = Delivery.find(params[:id])
   end
-  
+
+
   def update
+    delivery = Delivery.find(params[:id])
+    delivery.update(delivery_params)
+    redirect_to 'index'
   end
   
   
   
   
   
-  
+  private
+
+  def delivery_params
+    params.require(:delivery).permit(:postcode, :address, :name)
+  end
+
+
+
 end
