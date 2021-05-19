23c5e6f2e0d64086b7ea644fe1f871a63170bbe4
syokaturyou <teamsumagaku2@gmail.com>
2021-05-19 13:47:49 +0000

    管理者系更新1

--- app/controllers/manager/genres_controller.rb
+++ app/controllers/manager/genres_controller.rb
@@ -1,2 +1,26 @@
 class Manager::GenresController < ApplicationController
+    
+     def index
+     @genres=Genre.all
+     @newgenre = Genre.new
+     end
+     
+     def edit
+     @genre = Genre.find(params[:id])
+     end
+     
+     def update
+     end
+     
+     def create
+     genre = Genre.new(genre_params)
+     genre.save
+     redirect_to manager_genres_path
+     end
+
+     private
+     def genre_params
+     params.require(:genre).permit(:name)
+     end
+         
 end
