e5d3690ef6397abfb8d1ecef1f665f206930c51d
reiko-ishihara <violet794.py16@gmail.com>
2021-05-19 15:54:35 +0000

    add

--- app/controllers/public/deliveries_controller.rb
+++ app/controllers/public/deliveries_controller.rb
@@ -8,12 +8,16 @@ class Public::DeliveriesController < ApplicationController
 
   def create
     delivery = Delivery.new(delivery_params)
+    delivery.member_id = current_member.id
     delivery.save
-    redirect_to 'index'
+    redirect_to action: :index
   end
 
   def destroy
-    
+    delivery = Delivery.find(params[:id])
+    delivery.member_id = current_member.id
+    delivery.destroy
+    redirect_to action: :index
   end
 
   def edit
@@ -23,14 +27,13 @@ class Public::DeliveriesController < ApplicationController
 
   def update
     delivery = Delivery.find(params[:id])
+    delivery.member_id = current_member.id
     delivery.update(delivery_params)
-    redirect_to 'index'
+    redirect_to action: :index
   end
-  
-  
-  
-  
-  
+
+
+
   private
 
   def delivery_params
