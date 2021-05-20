13fedc5911c05d9928cd3e1621d47e579e589e49
reiko-ishihara <violet794.py16@gmail.com>
2021-05-20 12:36:55 +0000

    test

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -34,3 +34,5 @@ class Public::MembersController < ApplicationController
     params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
    end
 end
+
+
