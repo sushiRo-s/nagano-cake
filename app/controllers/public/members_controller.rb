5c3d88af2e243953fda0f2e65f06dc0b93e195da
Matsuoka-source <tm19930425@gmail.com>
2021-05-20 08:33:32 +0000

    会員情報

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,20 +1,23 @@
 class Public::MembersController < ApplicationController
-  before_action:authenticate_member
+  before_action:authenticate_member!
 
   def show
     @member = current_member
   end
 
   def edit
-   @member = Member.find(current_member.id)
+    @member = current_member
   end
 
-  # def update
-  #   @member = Member.find(current_member.id)
-  #   if @member.update(member_params)
-  #     redirect_to members_mems_path
-  #   else
-  #     render 'edit'
-  #   end
-  # end
+  def update
+    member = Member.find(params[:id])
+    member.update(member_params)
+    redirect_to public_member_path(member)
+  end
+
+  private
+
+   def member_params
+    params.require(:member).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :postcode, :address, :phone_number)
+   end
 end
