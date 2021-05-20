3fd82fda13a3e6bcd3057867fd600c2727e2d6d5
syokaturyou <teamsumagaku2@gmail.com>
2021-05-20 10:21:41 +0000

    puroduct系更新

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -1,3 +1,32 @@
 class Manager::ProductsController < ApplicationController
   before_action:authenticate_admin!
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def new
+   @product = Product.new
+  end
+  
+  def create
+    @product = Product.new(product_params)
+    @product.save
+    redirect_to manager_products_path
+  end
+  
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :image, :title)
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_17_113501) do
+ActiveRecord::Schema.define(version: 2021_05_20_092818) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 2021_05_17_113501) do
     t.boolean "is_active", default: true
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
   end
 
 end
