acad8aa50fda3c245edf040bd5147bd038ab8e21
reiko-ishihara <violet794.py16@gmail.com>
2021-05-24 07:22:09 +0000

    add

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -15,25 +15,25 @@ class Public::OrdersController < ApplicationController
   @carts.each do |cart|
   @sum += (cart.product.price * 1.1).floor * cart.quantity
   end
-  @order.total_price = @sum + @order.send_price
-  
-   
+  @order.total_price = @sum + 800
+
+
      if params[:delivery_method] == "0"
-        @order.delivery_postcode = @member.postcode  
-        @order.delivery_address =  @member.address  
+        @order.delivery_postcode = @member.postcode
+        @order.delivery_address =  @member.address
         @order.delivery_name =    (@member.last_name + @member.first_name)
-        
-     elsif params[:delivery_method]  == "1"  
+
+     elsif params[:delivery_method]  == "1"
            @delivery = Delivery.find(params[:order][:delivery_id])
-           @order.delivery_postcode = @delivery.postcode  
-           @order.delivery_address =  @delivery.address 
+           @order.delivery_postcode = @delivery.postcode
+           @order.delivery_address =  @delivery.address
            @order.delivery_name =     @delivery.name
-     elsif params[:delivery_method]  == "2"  
+     elsif params[:delivery_method]  == "2"
      else
            redirect_to new_public_order_path
      end
 
-  end  
+  end
 
   def complete
   end
@@ -46,12 +46,12 @@ class Public::OrdersController < ApplicationController
    @order = Order.find(params[:id])
    @orders = Order.all
   end
-  
+
   private
 
   def order_params
     params.require(:order).permit(:payment_method, :status, :delivery_name, :delivery_address, :delivery_postcode, :member_id, :member_address)
   end
-  
+
 
 end
