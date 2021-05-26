ea66f73dde768aaa6c50edd34efd4f1cd24cc7b2
Matsuoka-source <tm19930425@gmail.com>
2021-05-26 08:22:37 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,11 @@ class ApplicationController < ActionController::Base
 
 
     def after_sign_out_path_for(resource)
-      new_admin_session_path
+      if resource.is_a?(Admin)
+       new_admin_session_path
+      else resource.is_a?(Member)
+       root_path(resource)
+      end
     end
 
 
