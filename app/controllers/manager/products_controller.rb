9621916275bc1ebc0df22a89668ba75bdda6978d
syokaturyou <teamsumagaku2@gmail.com>
2021-05-26 08:27:01 +0000

    aa

--- app/controllers/manager/products_controller.rb
+++ app/controllers/manager/products_controller.rb
@@ -2,7 +2,7 @@ class Manager::ProductsController < ApplicationController
   before_action:authenticate_admin!
 
   def index
-    @products = Product.page(params[:page]).reverse_order
+    @products = Product.page(params[:page])
     @genres= Genre.all
   end
 
